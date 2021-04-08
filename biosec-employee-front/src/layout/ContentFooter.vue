55d0f566942e47a87696751319243d6ab1d701f7
shiftback3 <shiftback3@gmail.com>
2021-04-08 12:11:01 +0100

    first commit from local machine to git repo

---
+++ biosec-employee-front/src/layout/ContentFooter.vue
@@ -0,0 +1,28 @@
+<template>
+  <footer class="footer px-4">
+    <div class="row align-items-center justify-content-lg-between">
+      <div class="col-lg-6">
+        <div class="copyright text-center text-lg-left text-muted">
+          © {{ year }}
+          <a
+            href="https://www.biosec.com.ng"
+            class="font-weight-bold ml-1"
+            target="_blank"
+            >Biosec Solutions</a
+          >
+        </div>
+      </div>
+      
+    </div>
+  </footer>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      year: new Date().getFullYear(),
+    };
+  },
+};
+</script>
+<style></style>
